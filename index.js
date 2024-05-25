9a42f7db7976d133bc1087604ca95e3df56224ab
Shahzaib-ur-Rehman <shahzaib@hq.crunchcraft.com>
2024-05-25 20:56:33 +0500

    notifications count updated

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const toggleButton = document.getElementById("markAsReadBtn");
 toggleButton.addEventListener("click", () => {
   const items = document.querySelectorAll(".notications__list--item");
   const paragraphs = document.querySelectorAll(".unread-noticiations-icon");
+  const noticationsCount = document.getElementById("notificationsCount");
 
   // Loop through each item and remove specified classes
   items.forEach((item) => {
@@ -11,4 +12,5 @@ toggleButton.addEventListener("click", () => {
   paragraphs.forEach((item) => {
     item.classList.remove("unread-noticiations-icon");
   });
+  noticationsCount.innerText=0
 });
